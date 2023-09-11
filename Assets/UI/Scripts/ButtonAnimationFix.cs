1d8882d956dc3cc1849128ae52d932013dcf2208
Parham-Esmailzadeh <parhamesmailzadeh911@gmail.com>
2023-09-11 09:39:43 -0700

    init

---
+++ Assets/UI/Scripts/ButtonAnimationFix.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Michsky.UI.Dark
+{
+    public class ButtonAnimationFix : MonoBehaviour
+    {
+        private Button fixButton;
+
+        void Start()
+        {
+            fixButton = gameObject.GetComponent<Button>();
+            fixButton.onClick.AddListener(Fix);
+        }
+
+        public void Fix()
+        {
+            // bayad object hamono faal va gheir faal konim vararna rooye 
+            // on animation highlight gir mikone. moshkele man nis
+            // moshkele Unity hast :))))))
+            fixButton.gameObject.SetActive(false);
+            fixButton.gameObject.SetActive(true);
+        }
+    }
+}
