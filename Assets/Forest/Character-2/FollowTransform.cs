41a11a3bc541715de690e3972304486a524ce843
Parham-Esmailzadeh <parhamesmailzadeh911@gmail.com>
2023-10-19 08:39:51 -0700

    character & animation

---
+++ Assets/Forest/Character-2/FollowTransform.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(target.transform.position.x, 0f, -10f);
+    }
+}
---
+++ Assets/Forest/Scripts/Movement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    public bool jump = false;
+
+    public Animator animator;
+
+    public bool disabledJump = false;
+
+    //public Joystick joystick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Log()
+    {
+        Debug.Log("Landed");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        //horizontalMove = joystick.Horizontal * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if ( !disabledJump )
+            {
+                animator.SetBool("Jump", true);
+                disabledJump = true;
+                jump = true;
+            }
+        }
+    }
+    public void Landed()
+    {
+        disabledJump = false;
+        jump = false;
+    }
+    public void Jump()
+    {
+        jump = true;
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        // in balaye male paridane
+        // jump = false;
+        animator.SetBool("Jump", false);
+    }
+}
